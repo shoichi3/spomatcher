336988879d882e14414a67a4bb5dfca3176c2f29
shoichi3 <dece12shou@gmail.com>
2020-12-15 15:10:48 +0900

    usersテーブルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_051337) do
+ActiveRecord::Schema.define(version: 2020_12_15_053841) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_12_15_051337) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.text "profile"
-    t.string "profile_image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
