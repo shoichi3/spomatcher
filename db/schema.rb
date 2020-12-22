c4c049ae8f49691f51fb6761113992916285b710
shoichi3 <dece12shou@gmail.com>
2020-12-22 17:19:29 +0900

    Postsテーブルのカラム名を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_023451) do
+ActiveRecord::Schema.define(version: 2020_12_22_052121) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_023451) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.string "type", null: false
+    t.string "sports", null: false
     t.string "datetime", null: false
     t.string "regular_date"
     t.string "region", null: false
