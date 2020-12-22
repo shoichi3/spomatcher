86c3f972a5731cd2a73a44f12cf1a448726137ce
shoichi3 <dece12shou@gmail.com>
2020-12-22 17:21:20 +0900

    Postsテーブルのバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
+
   acts_as_taggable
   acts_as_taggable_on :skills, :interests
-  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :sports
+    validates :datetime
+    validates :region
+    validates :cost
+    validates :content
+    validates :flow
+  end
+
+  validates :image, presence: {message: 'を選択してください。'}
 end
