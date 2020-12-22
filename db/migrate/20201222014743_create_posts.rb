e0f0393e081d993f5f9df0531f4c74004c28408e
shoichi3 <dece12shou@gmail.com>
2020-12-22 10:57:21 +0900

    投稿機能のためPostテーブルをmigrate

--- db/migrate/20201222014743_create_posts.rb
+++ db/migrate/20201222014743_create_posts.rb
@@ -1,7 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title,      null: false
+      t.string :type,       null: false
+      t.string :datetime,   null: false
+      t.string :regular_date
+      t.string :region,     null: false
+      t.string :cost,       null: false
+      t.text :content,      null: false
+      t.text :flow,         null: false
+      t.references :user,   null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_053841) do
+ActiveRecord::Schema.define(version: 2020_12_22_014743) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,21 @@ ActiveRecord::Schema.define(version: 2020_12_15_053841) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "type", null: false
+    t.string "datetime", null: false
+    t.string "regular_date"
+    t.string "region", null: false
+    t.string "cost", null: false
+    t.text "content", null: false
+    t.text "flow", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -48,4 +63,5 @@ ActiveRecord::Schema.define(version: 2020_12_15_053841) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
