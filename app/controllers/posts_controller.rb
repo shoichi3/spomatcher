ed451a784a4c92c2d4b0c6fba640d2b6e17febfc
shoichi3 <dece12shou@gmail.com>
2020-12-22 11:35:44 +0900

    newアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+
+  def new
+    if user_signed_in?
+      @user = User.find_by(id: current_user.id)
+    else
+      redirect_to new_user_registration_path
+    end
+    @post = Post.new
+  end
+
 end
