f69e5a463acb47b872089f07a844e0ad95f5e1ca
shoichi3 <dece12shou@gmail.com>
2020-12-22 17:21:01 +0900

    新規投稿をPostsテーブルに追加するためのcreate,post_paramsアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      @user = User.find_by(id: current_user.id)
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image, :tag_list, :sports, :datetime, :regular_date, :region, :cost, :content, :flow).merge(user_id: current_user.id)
+  end
+
 end
