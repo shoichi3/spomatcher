beecd220c56f2bc7105ff6597c3a9418fcdf3e15
shoichi3 <dece12shou@gmail.com>
2020-12-31 14:03:15 +0900

    緯度及び経度を算出するための設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,14 @@ class Post < ApplicationRecord
   acts_as_taggable
   acts_as_taggable_on :skills, :interests
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   with_options presence: true do
     validates :title
     validates :sports
     validates :datetime
-    validates :region
+    validates :address
     validates :cost
     validates :content
     validates :flow
