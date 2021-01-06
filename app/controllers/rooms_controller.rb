03a6606389d344594cf98e423271336508b37f61
shoichi3 <dece12shou@gmail.com>
2021-01-06 18:15:33 +0900

    メッセージルーム作成のためのcreateおよびindexアクションの定義

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,18 @@
 class RoomsController < ApplicationController
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to rooms_path
+    end
+  end
+
+  def index
+    @user = User.find_by(id: current_user.id)
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(user_ids: [])
+  end
 end
