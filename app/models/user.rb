1e0296ff05e5cf7f58db98aa39cbfdab55f968ab
shoichi3 <dece12shou@gmail.com>
2021-01-06 17:16:36 +0900

    users、roomsおよびroom_usersテーブルのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
-  has_many :post
+  has_many :posts
+  has_many :room_users
+  has_many :rooms, through: :room_users
   
   validates :name, presence: true
   
