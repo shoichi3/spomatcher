dac2328e988669ce435d2292106937a4eda9e4e2
shoichi3 <dece12shou@gmail.com>
2021-01-07 11:17:23 +0900

    メッセージ画面に遷移するためのコントローラーの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,5 @@
 class MessagesController < ApplicationController
+  def index
+    @user = User.find_by(id: current_user.id)
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to rooms_path
+      redirect_to room_messages_path(@room.id)
     end
   end
 
