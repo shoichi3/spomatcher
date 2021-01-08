8287b455c544c1983d6d299367a524a45867775a
shoichi3 <dece12shou@gmail.com>
2021-01-08 11:37:13 +0900

    メッセージ画面のサイドバーの作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
   def index
     @user = User.find_by(id: current_user.id)
+    @rooms = @user.rooms
+    @room = Room.find(params[:room_id])
+    @message_partner = @room.users.where.not(id: current_user.id).first
   end
 end
