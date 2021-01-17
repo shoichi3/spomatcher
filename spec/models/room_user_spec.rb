143106dd7d3f20283c16061f4d9157119a2ff713
shoichi3 <dece12shou@gmail.com>
2021-01-17 13:19:11 +0900

    room_usersテーブルの単体テスト

--- spec/models/room_user_spec.rb
+++ spec/models/room_user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe RoomUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @room_user1 = build(:room_user)
+    @room_user2 = create(:room_user)
+    @room = build(:room)
+    @user = build(:user)
+  end
+
+  describe 'メッセージルーム作成機能' do
+    it 'ユーザーとルームがあれば保存されること' do
+      expect(@room_user1).to be_valid
+    end
+
+    it '1つのルームに同じユーザーが2回以上保存されないこと' do
+      @room_user2.valid?
+    end
+
+    it 'ユーザーが空のとき保存されないこと' do
+      @room.valid?
+    end
+
+    it 'ルームが空のとき保存されないこと' do
+      @user.valid?
+    end
+  end
 end
