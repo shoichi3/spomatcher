375a516f2846051629859091071ab920396750ab
shoichi3 <dece12shou@gmail.com>
2021-01-17 14:52:39 +0900

    結合テストにおけるログアウトおよびメッセージルーム作成動作のテンプレート化

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -10,14 +10,9 @@ RSpec.describe "メッセージルーム作成機能(サインイン後)", type:
     it '投稿者と異なるユーザーの場合、メッセージルームへ遷移することができる' do
       sign_up(@post.user)
       post(@post)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       sign_up(@user)
-      find('.post-link').click
-      expect{
-        click_on 'メッセージを送信'
-      }.to change { Room.count }.by(1)
-      expect(page).to have_content @post.user.name
+      make_room(@post.user)
     end
   end
 
@@ -32,14 +27,9 @@ RSpec.describe "メッセージルーム作成機能(サインイン後)", type:
     it '同様のユーザーと新たにメッセージルームを作成することはできないこと' do
       sign_up(@post.user)
       post(@post)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       sign_up(@user)
-      find('.post-link').click
-      expect{
-        click_on 'メッセージを送信'
-      }.to change { Room.count }.by(1)
-      expect(page).to have_content @post.user.name
+      make_room(@post.user)
       click_link 'Spomatcher'
       find('.post-link').click
       expect{
@@ -60,17 +50,11 @@ RSpec.describe 'メッセージルーム作成機能(ログイン後)', type: :s
     it '投稿者と異なるユーザーの場合、メッセージルームへ遷移することができること' do
       sign_up(@post.user)
       post(@post)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       sign_up(@user)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       log_in(@user)
-      find('.post-link').click
-      expect{
-        click_on 'メッセージを送信'
-      }.to change { Room.count }.by(1)
-      expect(page).to have_content @post.user.name
+      make_room(@post.user)
     end
   end
   
@@ -78,8 +62,7 @@ RSpec.describe 'メッセージルーム作成機能(ログイン後)', type: :s
     it '自身の投稿の場合、メッセージを送信というボタンは存在しない' do
       sign_up(@post.user)
       post(@post)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       log_in(@post.user)    
       find('.post-link').click
       expect(page).to have_no_content('メッセージを送信')
@@ -88,17 +71,11 @@ RSpec.describe 'メッセージルーム作成機能(ログイン後)', type: :s
     it '同様のユーザーと新たにメッセージルームを作成することはできないこと' do
       sign_up(@post.user)
       post(@post)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       sign_up(@user)
-      find('.user-image').click
-      click_on 'ログアウト'
+      log_out()
       log_in(@user)
-      find('.post-link').click
-      expect{
-        click_on 'メッセージを送信'
-      }.to change { Room.count }.by(1)
-      expect(page).to have_content @post.user.name
+      make_room(@post.user)
       click_link 'Spomatcher'
       find('.post-link').click
       expect{
