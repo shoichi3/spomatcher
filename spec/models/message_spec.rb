e5cb39572f3373523f0d94805dd9b639b233a013
shoichi3 <dece12shou@gmail.com>
2021-01-17 13:26:33 +0900

    messagesテーブルの単体テスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = build(:message)
+    @message.image = fixture_file_upload('app/assets/images/default.png')
+  end
+
+  describe 'メッセージ機能' do
+    it 'テキストと画像が存在するとき保存されること' do
+      expect(@message).to be_valid
+    end
+
+    it 'テキストが空白のとき保存されること' do
+      @message.text = nil
+      expect(@message).to be_valid
+    end
+
+    it '画像が空白のとき保存されること' do
+      @message.image = nil
+      expect(@message).to be_valid
+    end
+  end
 end
