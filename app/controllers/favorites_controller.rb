7124d9bb0c438e3dbd6fe800d4fad2c093fe51e6
shoichi3 <dece12shou@gmail.com>
2021-01-18 16:31:14 +0900

    お気に入り登録のためにcreateおよびdestoryアクションの設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,20 @@
 class FavoritesController < ApplicationController
+  before_action :set_post
+
+  def create
+    if @post.user_id != current_user.id
+      @favorite = Favorite.create(user_id: current_user.id, post_id: @post.id)
+    end
+  end
+
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
+    @favorite.destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
