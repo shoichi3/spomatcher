d62c6aa9abe2575215f2889d90c01a0c4135b39c
shoichi3 <dece12shou@gmail.com>
2021-01-18 12:45:03 +0900

    favoritesテーブルの作成

--- db/migrate/20210118034253_create_favorites.rb
+++ db/migrate/20210118034253_create_favorites.rb
@@ -1,7 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
-
+      t.references :user, foreign_key: true, null: false
+      t.references :post, foreign_key: true, null: false
       t.timestamps
     end
   end
