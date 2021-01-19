69c4bb9070cb4f5032c9c71a3578563893c9b785
shoichi3 <dece12shou@gmail.com>
2021-01-19 18:38:43 +0900

    マイページ作成のためのshowアクションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def show
+    if user_signed_in?
+      @user = User.find_by(id: current_user.id)
+    end
+    @postUser = User.find(params[:id])
+    @posts = @postUser.posts.order(created_at: "DESC")
+    @favorites = @postUser.favorites.includes(:user).order(created_at: "DESC")
+    @currentUserRoom = RoomUser.where(user_id: @user.id)
+    @postUserRoom = RoomUser.where(user_id: @postUser.id)
+    @currentUserRoom.each do |cu|
+      @postUserRoom.each do |po|
+        if cu.room_id == po.room_id
+          @haveRoom = true
+          @roomId = cu.room_id
+        end
+      end
+    end
+    unless @haveRoom
+      @room = Room.new
+    end
+  end
 end
