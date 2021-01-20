575697dcd1f07223e6bb904db9d079df5a8c6d3e
shoichi3 <dece12shou@gmail.com>
2021-01-20 15:40:30 +0900

    user及びassociationモデルのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   has_many :rooms, through: :room_users
   has_many :messages
   has_many :favorites
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship"
+   has_many :following, through: :following_relationships
+   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship"
+   has_many :followers, through: :follower_relationships
   
   validates :name, presence: true
   
@@ -37,5 +41,20 @@ class User < ApplicationRecord
     result = update_attributes(params, *options)
     clean_up_passwords
     result
+  end
+
+  #フォローしているかを確認するメソッド
+  def following?(user)
+    following_relationships.find_by(following_id: user.id)
+  end
+
+  #フォローするときのメソッド
+  def follow(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  #フォローを外すときのメソッド
+  def unfollow(user)
+    following_relationships.find_by(following_id: user.id).destroy
   end
 end
