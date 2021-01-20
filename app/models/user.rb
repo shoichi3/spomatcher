0f055d7fdb1570da781534bb9962f4909c66fdbe
shoichi3 <dece12shou@gmail.com>
2021-01-20 16:50:03 +0900

    アソシエーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :favorites
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship"
-   has_many :following, through: :following_relationships
+   has_many :followings, through: :following_relationships
    has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship"
    has_many :followers, through: :follower_relationships
   
