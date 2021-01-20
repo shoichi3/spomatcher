f83f5d8def05d37c0cdd691a4397a861df48f589
shoichi3 <dece12shou@gmail.com>
2021-01-20 15:44:08 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
 
   root "homes#index"
   resources :posts do
