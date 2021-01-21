f06f1d68367b400dadc591d667da78605ea57542
shoichi3 <dece12shou@gmail.com>
2021-01-21 12:30:12 +0900

    フォロー機能を実装するためのコントローラーの設定

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,34 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:followings, :followers, :destroy]
+
+  def followings
+    if user_signed_in?
+      @user = User.find_by(id: current_user.id)
+    end
+    @followings = @postUser.followings
+    @followers = @postUser.followers
+  end
+
+  def followers
+    if user_signed_in?
+      @user = User.find_by(id: current_user.id)
+    end
+    @followings = @postUser.followings
+    @followers = @postUser.followers
+  end
+
+  def create
+    @following_user = User.find(params[:following_id])
+    current_user.follow(@following_user)
+  end
+
+  def destroy
+    current_user.unfollow(@postUser)
+  end
+
+  private
+
+  def set_user
+    @postUser = User.find(params[:id])
+  end
 end
