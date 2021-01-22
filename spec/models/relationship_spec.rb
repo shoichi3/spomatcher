58bbd79e5e726642b19d3bd601dd37ece1b0e16e
shoichi3 <dece12shou@gmail.com>
2021-01-22 10:38:11 +0900

    フォロー機能における単体テストの導入

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @follower = create(:user)
+    @following = create(:user)
+    @relationship1 = build(:relationship, follower: @follower, following: @following)
+    @relationship2 = create(:relationship)
+  end
+  
+  describe 'フォロー機能' do
+    it 'followerとfollowingが存在すれば保存できること' do
+      expect(@relationship1).to be_valid
+    end
+
+    it '同じfollowerとfollowingの組み合わせの場合、保存できないこと' do
+      @relationship2.valid?
+    end
+
+    it 'followingが空のとき保存できないこと' do
+      @follower.valid?
+    end
+
+    it 'followerが空のとき保存できないこと' do
+      @following.valid?
+    end
+  end
 end
