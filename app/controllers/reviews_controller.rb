1139234ed97663b8715ff5eb089aa78898eab84b
shoichi3 <dece12shou@gmail.com>
2021-01-23 11:16:51 +0900

    レビューの一覧表示と新規作成のためのアクションの定義

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  before_action :set_user
+  
+  def index
+    @user = User.find_by(id: current_user.id)
+  end
+
+  def new
+    @user = User.find_by(id: current_user.id)
+    @review = Review.new
+  end
+
+  private
+  
+  def set_user
+    @user = User.find_by(id: current_user.id)
+  end
 end
