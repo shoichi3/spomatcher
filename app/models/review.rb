b9dd9924d9f481ff910005bb49dccdab9281b38c
shoichi3 <dece12shou@gmail.com>
2021-01-24 17:06:16 +0900

    エラー文を日本語にするための変更

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  validates :content, presence: true
-  validates :score, presence: true
   belongs_to :reviewer, class_name: "User", foreign_key: "reviewer_id"
   belongs_to :reviewee, class_name: "User", foreign_key: "reviewee_id"
+  validates :score, presence: true
+  validates :content, presence: true
 end
