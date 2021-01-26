df3359e98de5e7418f1d99754771fb94229b1c86
shoichi3 <dece12shou@gmail.com>
2021-01-26 10:33:00 +0900

    レビュー機能における単体テストの導入

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :review do
-    
+    content           { Faker::Lorem.characters(number: 30) }
+    score             { Faker::Number.between(from:1, to:5) }
+    association :reviewer, factory: :user
+    association :reviewee, factory: :user
   end
 end
