c5c9c52a10f3848a7ebdd18a57e1c7edecd7126f
shoichi3 <dece12shou@gmail.com>
2021-01-27 13:16:10 +0900

    メッセージを送信したときに送信相手に通知が届くようにするための追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,16 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   has_one_attached :image
+
+  def create_notification_message!(current_user, user, room)
+    notification = current_user.active_notifications.new(
+      visited_id: user.id,
+      room_id: room.id,
+      action: "message"
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
