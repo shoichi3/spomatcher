ceece77091844921a00bb0d395b2530859c8052b
shoichi3 <dece12shou@gmail.com>
2021-01-27 10:37:55 +0900

    Notificationモデルおよびテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_005423) do
+ActiveRecord::Schema.define(version: 2021_01_27_013143) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
