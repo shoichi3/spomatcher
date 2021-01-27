edb59258bd5a7ec285c2625b71fe8367fe8f8194
shoichi3 <dece12shou@gmail.com>
2021-01-27 13:42:57 +0900

    ユーザーに通知を知らせるためのコントローラーおよびビューの設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,9 @@
 class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.passive_notifications.order(created_at: "DESC")
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+    @user = User.find_by(id: current_user.id)
+  end
 end
