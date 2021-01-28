99855329a30fadc937f511c70b0b308692fc7acc
shoichi3 <dece12shou@gmail.com>
2021-01-28 17:07:10 +0900

    レビューが投稿されたときに通知が届くようにするための追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,8 @@ class Notification < ApplicationRecord
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
   belongs_to :post, optional: true
   belongs_to :room, optional: true
+  belongs_to :review, optional: true
+
+  ACTION_VALUES = %w(message favorite follow review).freeze
+  validates :action, presence: true, inclusion: { in: ACTION_VALUES }
 end
