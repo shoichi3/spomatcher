31a5e3a29e07b3e54ef0c6a33b6302089d75ae0c
shoichi3 <dece12shou@gmail.com>
2021-01-28 18:07:13 +0900

    レビュー機能における結合テストの部分テンプレート化

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,7 @@ RSpec.configure do |config|
   config.include MypageSupport
   config.include MessageSupport
   config.include FollowSupport
+  config.include ReviewSupport
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
