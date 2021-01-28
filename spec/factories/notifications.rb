410946cc0fbe70528316151eaacacca053c25d24
shoichi3 <dece12shou@gmail.com>
2021-01-28 17:07:55 +0900

    通知機能における単体テストの導入

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :notification do
-    
+    checked       { true }
+    action        { "message" } 
+    association :visitor, factory: :user
+    association :visited, factory: :user
+    association :post
+    association :room
   end
 end
