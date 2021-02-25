158de1e591f3f64d6f4c2e3746458bd33b2e7384
shoichi3 <dece12shou@gmail.com>
2021-02-25 14:43:01 +0900

    ゲストログイン機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
   def self.guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now
+      user.confirmed_at = Time.now
     end
   end
   
