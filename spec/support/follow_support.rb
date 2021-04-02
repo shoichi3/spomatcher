7690532dc2290f65a6aa419367174414aafe2e39
shoichi3 <dece12shou@gmail.com>
2021-04-02 12:07:28 +0900

    フォロー機能の結合テストの修正

--- spec/support/follow_support.rb
+++ spec/support/follow_support.rb
@@ -2,6 +2,6 @@ module FollowSupport
   def follow
     find('.icon').click
     find('#follow_form').click
-    expect(page).to have_css '.text-white'
+    expect(page).to have_content('フォロー中')
   end
 end
