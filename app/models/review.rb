b5826db7115c7e9a4b86d4b96a99849ead0fc75e
shoichi3 <dece12shou@gmail.com>
2021-04-05 13:55:12 +0900

    親モデルと共に子モデルを削除するためにdependent: :destroyの付与 [ci skip]

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :reviewer, class_name: 'User', foreign_key: 'reviewer_id'
   belongs_to :reviewee, class_name: 'User', foreign_key: 'reviewee_id'
-  has_many :notifications
+  has_many :notifications, dependent: :destroy
 
   validates :score, presence: true
   validates :content, presence: true
